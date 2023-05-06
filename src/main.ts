2ef1a140c060e03cf67b8f72fba0f6312ddbcf29
juandtap <104116727+juandtap@users.noreply.github.com>
2023-05-05 20:29:27 -0500

    agregado header y rightbar

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RightSideBarComponent } from './right-side-bar/right-side-bar.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HeaderComponent, RightSideBarComponent],
   templateUrl: './main.html',
   //styleUrls:['./main.css'],
 })
 export class App {
-  name = 'diego';
-  title = 'Calculadora';
-  contador = 0;
-  a = 20;
-  b = 10;
-  c = 0;
-
-  suma = 0;
-  resta = 0;
-  producto = 0;
-  division = 0;
-
-  calcular() {
-    this.contador = this.contador + 1;
-    console.log('calculando operaciones: ' + this.a+ ", "+this.b);
-    // suma
-    this.suma = (+this.a ) + (+this.b);
-    //resta
-    this.resta = this.a - this.b
-    // producto
-    this.producto = this.a *  this.b
-    // division
-    this.division = this.a / this.b
-  }
+  
 }
 
 bootstrapApplication(App);
---
+++ src/right-side-bar/right-side-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-side-bar',
+  templateUrl: './right-side-bar.component.html',
+  styleUrls: ['./right-side-bar.component.css'],
+  standalone: true,
+})
+export class RightSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
